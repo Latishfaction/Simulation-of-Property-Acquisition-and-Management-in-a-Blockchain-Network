e08d21b57d05786bfb3d7a39d554802e84c90377
Latishfaction <latishchambhare@gmail.com>
2023-03-01 00:17:48 +0530

    set up MuncipalCorporation DB

--- POAM/Aadhar_DB/views.py
+++ POAM/Aadhar_DB/views.py
@@ -7,33 +7,33 @@ from django.urls import reverse
 from .models import Person,aadhar
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello")
+# def index(request):
+#     return HttpResponse("Hello")
 
 # for temporary query of data base
-def login_view(request):
-    if request.method == "POST":
+# def login_view(request):
+#     if request.method == "POST":
 
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
+#         # Attempt to sign user in
+#         username = request.POST["username"]
+#         password = request.POST["password"]
+#         user = authenticate(request, username=username, password=password)
 
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "Aadhar_DB/login.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "Aadhar_DB/login.html")
+#         # Check if authentication successful
+#         if user is not None:
+#             login(request, user)
+#             return HttpResponseRedirect(reverse("index"))
+#         else:
+#             return render(request, "Aadhar_DB/login.html", {
+#                 "message": "Invalid username and/or password."
+#             })
+#     else:
+#         return render(request, "Aadhar_DB/login.html")
 
 
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("index"))
+# def logout_view(request):
+#     logout(request)
+#     return HttpResponseRedirect(reverse("index"))
 
 
 # def register(request):
@@ -64,32 +64,32 @@ def logout_view(request):
 #     else:
 #         return render(request, "Aadhar_DB/register.html")
 
-def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
+# def register(request):
+#     if request.method == "POST":
+#         username = request.POST["username"]
 
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "Aadhar_DB/register.html", {
-                "message": "Passwords must match."
-            })
-        otp = request.POST["otp"]
-        # get the aadhar card by the username
-        u1 = aadhar.objects.get(aadhar_no = int(username))
+#         # Ensure password matches confirmation
+#         password = request.POST["password"]
+#         confirmation = request.POST["confirmation"]
+#         if password != confirmation:
+#             return render(request, "Aadhar_DB/register.html", {
+#                 "message": "Passwords must match."
+#             })
+#         otp = request.POST["otp"]
+#         # get the aadhar card by the username
+#         u1 = aadhar.objects.get(aadhar_no = int(username))
 
-        print(u1)
-        # put the aadhar card data in the user model
-        try :
-            user = Person(username=u1,password=password,otp=otp)
-            user.save()
-            print("Saved")
-        except IntegrityError:
-            return render(request, "Aadhar_DB/register.html", {
-                "message": "Username already taken."
-            })
+#         print(u1)
+#         # put the aadhar card data in the user model
+#         try :
+#             user = Person(username=u1,password=password,otp=otp)
+#             user.save()
+#             print("Saved")
+#         except IntegrityError:
+#             return render(request, "Aadhar_DB/register.html", {
+#                 "message": "Username already taken."
+#             })
 
-        return HttpResponse("Done")
-    else:
-        return render(request,"Aadhar_DB/register.html")
+#         return HttpResponse("Done")
+#     else:
+#         return render(request,"Aadhar_DB/register.html")
--- POAM/MuncipalCorporation_DB/models.py
+++ POAM/MuncipalCorporation_DB/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from Aadhar_DB.models import Person
 # Create your models here.
+class Plot(models.Model):
+    SQFT = 'sqft'
+    ACRE = 'AC'
+    units = [
+        (SQFT, 'Square.ft'),
+        (ACRE, 'Acre'),
+    ]
+    owner = models.ForeignKey(Person,on_delete=models.CASCADE,related_name="plot_owner")
+    number = models.IntegerField()
+    area = models.DecimalField(max_digits=10, decimal_places=2)
+    length= models.DecimalField(max_digits=10, decimal_places=2)
+    width= models.DecimalField(max_digits=10, decimal_places=2)
+    unit = models.CharField(choices=units,default=SQFT,max_length=4)
+    society_name= models.CharField(max_length=100)
+    society_rera_no = models.IntegerField()
+    plot_address = models.CharField(max_length=200)
+    north = models.CharField(max_length=30)
+    south = models.CharField(max_length=30)
+    east = models.CharField(max_length=30)
+    west = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.number} | {self.owner}"
