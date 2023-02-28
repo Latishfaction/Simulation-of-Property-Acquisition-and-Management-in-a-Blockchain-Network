12f8e51146212f6471112b60f94997c6cccfa94a
Latishfaction <latishchambhare@gmail.com>
2023-03-01 01:53:20 +0530

    set up all tthings

--- POAM/LegalComplaints_DB/models.py
+++ POAM/LegalComplaints_DB/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
+from MuncipalCorporation_DB.models import Plot
 # Create your models here.
+class Complaint(models.Model):
+    property_no = models.ForeignKey(Plot,on_delete=models.CASCADE,related_name="case_no")
--- POAM/Sarathi_DB/models.py
+++ POAM/Sarathi_DB/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from MuncipalCorporation_DB.models import Plot
+from LegalComplaints_DB.models import Complaint
 # Create your models here.
 
 class Plot_details(models.Model):
     details = models.ForeignKey(Plot,related_name="plot_details",on_delete=models.CASCADE)
-    is_blocked = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{details.number} | {details.owner}"
+
+class blacklist(models.Model):
+    Property_case = models.ForeignKey(Complaint,on_delete=models.CASCADE,related_name="proprty_case")
+    is_blocked = models.BooleanField(default=False)
