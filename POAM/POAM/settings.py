df21f0d33485978a8cd8cd77d957f1810a3ae270
Latishfaction <latishchambhare@gmail.com>
2023-02-28 23:30:35 +0530

    sarathi DB

--- POAM/POAM/settings.py
+++ POAM/POAM/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "Aadhar_DB",
+    "Sarathi_DB",
 ]
 
 MIDDLEWARE = [
---
+++ POAM/Sarathi_DB/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SarathiDbConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Sarathi_DB"
