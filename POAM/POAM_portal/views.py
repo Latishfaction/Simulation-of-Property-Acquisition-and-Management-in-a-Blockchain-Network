7aa85cf07a06e098b0b1f5166c46588284739f36
Latishfaction <latishchambhare@gmail.com>
2023-03-26 19:47:32 +0530

    setting up

--- POAM/POAM_portal/views.py
+++ POAM/POAM_portal/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.db import IntegrityError
+
 # Create your views here.
-from Aadhar_DB.models import aadhar,User
+from Aadhar_DB.models import aadhar, User
 from .models import Person
+
 # from .models import Person
 
-#register the person from aadhar_db to POAM_portal
+# register the person from aadhar_db to POAM_portal
 # authentication/views.py
 
 from . import forms
@@ -17,21 +19,23 @@ from . import forms
 
 def login_view(request):
     form = forms.LoginForm()
-    message = ''
-    if request.method == 'POST':
+    message = ""
+    if request.method == "POST":
         form = forms.LoginForm(request.POST)
         if form.is_valid():
             user = authenticate(
-                username= form.cleaned_data['username'],
-                password=form.cleaned_data['password'],
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
             )
             if user is not None:
                 login(request, user)
-                message = f'Hello {user.username.first_name}! You have been logged in'
+                message = f"Hello {user}! You have been logged in"
             else:
-                message = 'Login failed!'
+                message = "Login failed!"
     return render(
-        request, 'POAM_portal/login.html', context={'form': form, 'message': message})
+        request, "POAM_portal/login.html", context={"form": form, "message": message}
+    )
+
 
 def logout_view(request):
     logout(request)
@@ -47,26 +51,30 @@ def register(request):
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "Aadhar_DB/register.html", {
-                "message": "Passwords must match."
-            })
+            return render(
+                request, "Aadhar_DB/register.html", {"message": "Passwords must match."}
+            )
 
         # Attempt to create new user
         try:
-            user = User.objects.create_user(username, email,password)
+            user = User.objects.create_user(username, email, password)
             person = Person()
             person.user = user
-            person.aadhar_details = aadhar.objects.get(aadhar_no = int(username)) 
+            person.aadhar_details = aadhar.objects.get(aadhar_no=int(username))
             user.save()
             person.save()
         except IntegrityError:
-            return render(request, "POAM_portal/register.html", {
-                "message": "Already Registered."
-            })
-        except (Exception):
-            return render(request, "POAM_portal/register.html", {
-                "message": Exception,
-            })
+            return render(
+                request, "POAM_portal/register.html", {"message": "Already Registered."}
+            )
+        except Exception:
+            return render(
+                request,
+                "POAM_portal/register.html",
+                {
+                    "message": Exception,
+                },
+            )
         login(request, user)
         return HttpResponse("Registered and login successfully!")
     else:
