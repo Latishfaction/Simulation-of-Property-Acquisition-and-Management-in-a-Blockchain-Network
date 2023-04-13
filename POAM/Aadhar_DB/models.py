ec3da5724763b009f629bce474b6fe5057348680
Latishfaction <latishchambhare@gmail.com>
2023-04-13 20:58:26 +0530

    #

--- POAM/Aadhar_DB/models.py
+++ POAM/Aadhar_DB/models.py
@@ -23,6 +23,7 @@ class aadhar(models.Model):
     aadhar_no = models.BigIntegerField()
     address = models.TextField()
     photo = models.ImageField(upload_to ='uploads/',null=True)
+    mobile_number = models.CharField(max_length=10,null=True)
 
     def __str__(self):
         return f"Aadhar No : {self.aadhar_no} DOB : {self.dob}"
